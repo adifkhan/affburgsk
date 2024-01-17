815eba7e6172e51ae4f21d07b66f69b82a68bf8b
Jubair Ahammed Tolon <zs.tolon0073@gamil.com>
2024-01-17 16:51:48 +0600

    campaign manager done

--- app/(dashboard)/(routes)/manageCampaign/page.tsx
+++ app/(dashboard)/(routes)/manageCampaign/page.tsx
@@ -3,6 +3,7 @@
 import { Box, Button, Card, CardContent, Typography } from '@mui/material';
 import { useRouter } from 'next/navigation';
 import React from 'react'
+import { MdInfoOutline } from 'react-icons/md';
 
 
 export default function ManageCampaign() {
@@ -46,7 +47,7 @@ export default function ManageCampaign() {
                 backgroundColor: '#1C2437', mt: 2, color: '#36A689'
             }}>
                 <CardContent sx={{ display: 'flex', flexDirection: 'column', gap: 1.5, }}>
-                    <em style={{ fontWeight: 600 }}>Notice:</em>
+                    <em style={{ fontWeight: 600, display: 'flex', alignItems: 'center', gap: 1 }}><MdInfoOutline />Notice:</em>
                     <Typography variant='body2' sx={{ fontSize: 12 }}>{"The Affiliate Marketing industry is VERY competitive. If you choose to disclose to an affiliate network that you are sending CPAlead's traffic to their campaigns, they may block your account with a fake reason and keep the money you earned. Please ask the network that provided you with your campaigns for feedback on the traffic/leads you receive from CPAlead, preferably every 24 hours to avoid a loss. There have been multiple instances where affiliate networks are happy with the traffic they are receiving however they will ban your account as soon as they are told the traffic is coming from CPAlead (their competitor). It is the perfect opportunity for them to make a competitor look bad (us) while stealing your money. We have taken multiple steps to obscure our tracking links to avoid networks banning your account for this reason."}</Typography>
                 </CardContent>
             </Card>
